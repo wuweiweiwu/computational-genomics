151e1c88806fc0e608b3b0efed7b644c86b64a2a
hungweiwu <wuxx1045@umn.edu>
2017-11-17 11:11:22 -0600

    adding bootstrap

--- hw3/README.txt
+++ hw3/README.txt
@@ -25,3 +25,35 @@ Rscript hw3-plot-newick.r tree.txt hw3-tip-labels.txt
 or
 Rscript hw3-plot-edges.r edges.txt hw3-tip-labels.txt
 ```
+
+
+### Bonus: bootstrapping
+
+uncomment code in main() because it takes forever to run
+
+line 338 - 339
+
+```
+percentages = bootstrap(root, ids, id_sequences)
+write_bootstrap(percentages)
+```
+
+Visualize
+
+```
+Rscript hw3-plot-edges.r edges.txt hw3-tip-labels.txt bootstrap.txt
+```
+
+
+### Files
+
+
+```
+edges.txt
+tree.txt
+bootstrap.txt
+
+newick-tree.pdf
+edges-tree.pdf
+bootstrap-tree.pdf
+```
--- hw3/main.py
+++ hw3/main.py
@@ -1,6 +1,6 @@
 from tree import TreeNode
 import sys
-
+import random
 
 # function to calculate the difference between two input id_sequences
 # simply counts mismatches and return the percentage as a float
@@ -229,19 +229,114 @@ def nei_saitou(original_ids, original_distance_matrix):
     return rootNode
 
 
+# get the leaves under this node using dfs
+# returns a set of node.ids
+def get_leaves(node):
+    leaves = set()
+    def dfs(node):
+        global count
+        if not node or not node.children:
+            leaves.add(node.id)
+        else:
+            for child in node.children.keys():
+                dfs(child)
+    dfs(node)
+    return leaves
+
+
+# return the dictionary of id -> set of leaves under that node
+# using bfs with a queue
+def get_partitions(root):
+    queue = [root]
+    partition = {}
+    while len(queue):
+        tmp = []
+        for node in queue:
+            # this is a leaf
+            if not node.children:
+                continue
+            leaves = get_leaves(node)
+            partition[node.id] = leaves
+            for child in node.children.keys():
+                tmp.append(child)
+        queue = tmp
+    return partition
+
+
+# get the dfs order of the tree
+def get_id_order(root):
+    order = []
+    def dfs(node):
+        if not node or not node.children:
+            return
+        else:
+            order.append(node.id)
+            for child in node.children.keys():
+                dfs(child)
+    dfs(root)
+    return order
+
+
+# do the 100 bootstrap sampling
+def bootstrap(original_root, ids, id_sequences):
+    # original order of teh ids in the original tree (dfs)
+    original_order = get_id_order(original_root)
+    original_partitions = get_partitions(original_root)
+    partition_count = [0] * 59
+    for _ in xrange(100):
+        # new bootstrap sequence
+        bootstrap_sequences = {}
+        # the columns to swap
+        all_indices = [i for i in xrange(len(id_sequences['152801']))]
+        indices = [random.choice(all_indices) for _ in xrange(len(all_indices))]
+        for id in ids:
+            # choose randomly the to reconstruct the sequence
+            new_sequence = ''
+            for index in indices:
+                new_sequence += id_sequences[id][index]
+            bootstrap_sequences[id] = new_sequence
+        # do the nei_saitou and get a new tree
+        distance_matrix = get_difference_matrix(ids, bootstrap_sequences)
+        root = nei_saitou(ids, distance_matrix)
+        # get the dictionary of partitions and ids under those partitions
+        partitions = get_partitions(root)
+
+        # compare partitions to see which trees make the same partition as the original
+        for index, id in enumerate(original_order):
+            if original_partitions[id] == partitions[id]:
+                partition_count[index] += 1
+    percentages = [count / 100.0 for count in partition_count]
+    return percentages
+
+# write the percentages in the bootstrap file
+def write_bootstrap(percentages):
+    with open('bootstrap.txt', 'w') as f:
+        for percent in percentages:
+            f.write(str(percent) + '\n')
+
+
 def main(filename):
     # read the fna file and return the ids and id sequence mappings
     ids, id_sequences = read_fasta_file(filename)
+
     # generate the difference matrix
     distance_matrix = get_difference_matrix(ids, id_sequences)
+
     # write the distances.txt file (distance matrix)
     write_distance_file(ids, distance_matrix)
+
     # run the nei saitu algorithm. Returns the root of the tree
     root = nei_saitou(ids, distance_matrix)
+
     # write the edge file using preorder traversal
     write_edge_file(root)
+
     # write the newick file using postorder traversal
     write_newick_file(ids, root)
+
+    # bootstrap bonus points
+    percentages = bootstrap(root, ids, id_sequences)
+    write_bootstrap(percentages)
 
 
 if __name__ == '__main__':

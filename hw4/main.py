641368a9ba6cb64ee40a16559e75daf088e1ce22
hungweiwu <wuxx1045@umn.edu>
2017-11-27 15:58:51 -0600

    maybe calculating variability correctly?

--- hw4/main.py
+++ hw4/main.py
@@ -2,6 +2,7 @@ import collections
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # function to read the fna file. Returns the ids and the dictionary
 # of relationships of id -> sequence for fast lookups
 def read_fasta_file(filename):
@@ -17,8 +18,7 @@ def read_fasta_file(filename):
     return dictionary
 
 
-def main():
-    dictionary = read_fasta_file('seqs.fna')
+def calculate_variability(dictionary):
     y = []
     for i in xrange(7682):
         count = collections.defaultdict(int)
@@ -31,13 +31,39 @@ def main():
             for value in count.values():
                 total += value
             maxKey = max(count, key=count.get)
-            y.append(count[maxKey]/float(total))
-        else:
-            y.append(0)
-    x = np.arange(1, 7683)
-    y = np.array(y)
-    plt.plot(x, y, "o")
+            y.append(count[maxKey] / float(total))
+    return y
+
+
+def write_variability(variabilities):
+    with open('variability.txt', 'w') as f:
+        for variability in variabilities:
+            f.write(str(variability) + '\n')
+
+
+def read_variability():
+    y = []
+    with open('variability.txt') as f:
+        content = f.read().splitlines()
+    for index, line in enumerate(content):
+        y.append(float(line))
+    return y
+
+
+def plot_variability(variability):
+    x = np.arange(1, len(variability) + 1)
+    y = np.array(variability)
+    plt.plot(x, y, linewidth=1.0)
     plt.show()
+
+
+def main():
+    # dictionary = read_fasta_file('seqs.fna')
+    # variabilities = calculate_variability(dictionary)
+    # write_variability(variabilities)
+    y = read_variability()
+    plot_variability(y)
+
 
 if __name__ == '__main__':
     main()

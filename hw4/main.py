8529fa84f3ac410ed5585cd2b8f59afa275ae9d4
hungweiwu <wuxx1045@umn.edu>
2017-11-27 01:48:51 -0600

    trying to figure out % identity

--- hw4/main.py
+++ hw4/main.py
@@ -1,23 +1,43 @@
+import collections
+import numpy as np
+import matplotlib.pyplot as plt
+
 # function to read the fna file. Returns the ids and the dictionary
 # of relationships of id -> sequence for fast lookups
 def read_fasta_file(filename):
     dictionary = {}
-    ids = []
     with open(filename) as f:
         content = f.read().splitlines()
     currId = None
     for index, line in enumerate(content):
         if index % 2 == 0:
             currId = line[1:]
-            ids.append(currId)
         else:
             dictionary[currId] = line
-    return ids, dictionary
+    return dictionary
 
 
 def main():
-    read_fasta_file('seqs.fna')
-
+    dictionary = read_fasta_file('seqs.fna')
+    y = []
+    for i in xrange(7682):
+        count = collections.defaultdict(int)
+        for key, value in dictionary.items():
+            if value[i] == '-':
+                continue
+            count[value[i]] += 1
+        if count:
+            total = 0
+            for value in count.values():
+                total += value
+            maxKey = max(count, key=count.get)
+            y.append(count[maxKey]/float(total))
+        else:
+            y.append(0)
+    x = np.arange(1, 7683)
+    y = np.array(y)
+    plt.plot(x, y, "o")
+    plt.show()
 
 if __name__ == '__main__':
     main()
